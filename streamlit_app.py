bd5c3730f0a6b0186e49f77d25d181ab678ef35b
Qodor04 <qaidar436@gmail.com>
2025-07-20 20:27:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,7 +198,7 @@ def colored_progress_bar(progress, bar_color_var):
     </div>""", unsafe_allow_html=True)
 
 with st.sidebar:
-    st.image("https://photos.google.com/photo/AF1QipM_N2xF4eeu-VKTnGL1vBcRTmsJoME7QabgK6NG", width=100)
+    st.image("https://freeimage.host/i/FwEQAdX", width=100)
     st.title("GluPal")
     if 'tracker' in st.session_state and st.session_state.tracker.user_profile:
         user_name = st.session_state.tracker.user_profile.get('nama', "Pengguna")
